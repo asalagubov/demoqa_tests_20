8dbfdf8be328b0752da1e9945404ea2f58320f69
Aleksandr Salagubov <salagubov@icloud.com>
2023-06-14 19:00:09 +0300

    update

--- src/test/java/demoqa/Demoqa.java
+++ src/test/java/demoqa/Demoqa.java
@@ -15,7 +15,7 @@ public class Demoqa {
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = " https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.holdBrowserOpen = true;
+        // Configuration.holdBrowserOpen = true;
     }
     @Test
     void successTest() {

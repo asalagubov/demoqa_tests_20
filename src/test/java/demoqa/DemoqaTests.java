d0a9c77905debe05fc4e39af7ae7604cb0f81881
Aleksandr Salagubov <salagubov@icloud.com>
2023-06-18 15:28:11 +0300

    added new file and rename other tests

--- src/test/java/demoqa/DemoqaTests.java
+++ src/test/java/demoqa/DemoqaTests.java
@@ -1,44 +1,64 @@
 package demoqa;
 
-
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import demoqa.pages.RegistrationPage;
 
-public class DemoqaTests extends TestBase {
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
-    RegistrationPage registrationPage = new RegistrationPage();
+public class DemoqaTests {
 
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = " https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        // Configuration.holdBrowserOpen = true;
+    }
     @Test
     void successTest() {
 
-        registrationPage
-                .openPage()
-                .closeBanners()
-                .setFirstName("Александр")
-                .setLastName("Ваш")
-                .setUserEmail("test@mail.ru")
-                .setGender("Other")
-                .setUserNumber("9999999999")
-                .setBirthDay("01", "January", "1997")
-                .setSubjects("Arts")
-                .setHobbies("Music", "Sports")
-                .setUploadPicture("3x.png")
-                .setAddress("Москва, Украинский бульвар, д. 6")
-                .setState("NCR")
-                .setCity("Delhi")
-                .setSubmit();
+        open("/automation-practice-form");
 
-        registrationPage
-                .verifyThanksText()
-                .verifyResult("Student Name", "Александр Ваш")
-                .verifyResult("Student Email", "test@mail.ru")
-                .verifyResult("Gender", "Other")
-                .verifyResult("Mobile", "9999999999")
-                .verifyResult("Date of Birth", "01 January,1997")
-                .verifyResult("Subjects", "Arts")
-                .verifyResult("Hobbies", "Music, Sports")
-                .verifyResult("Picture", "3x.png")
-                .verifyResult("Address", "Москва, Украинский бульвар, д. 6")
-                .verifyResult("State and City", "NCR Delhi");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        $("#firstName").setValue("Александр");
+        $("#lastName").setValue("Ваш");
+        $("#userEmail").setValue("test@mail.ru");
+        $("#genterWrapper").$(byText("Other")).click();
+        $("#userNumber").setValue("9999999999");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionByValue("0");
+        $(".react-datepicker__year-select").selectOptionByValue("1997");
+        $(".react-datepicker__day--001").click();
+        $("#subjectsInput").setValue("A");
+        $(byText("Arts")).click();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("3x.png");
+        $("#currentAddress").setValue("Москва, Украинский бульвар, д. 6");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+        $(".modal-header").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("Александр Ваш"));
+        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text("test@mail.ru"));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Other"));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("9999999999"));
+        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("01 January,1997"));
+        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("Arts"));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Music, Sports"));
+        $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("3x.png"));
+        $(".table-responsive").$(byText("Address")).parent().shouldHave(text("Москва, Украинский бульвар, д. 6"));
+        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("NCR Delhi"));
+
     }
+
+
 }

7064409c62d8dfd941c33ae031b40fbaec2f9657
Aleksandr Salagubov <salagubov@icloud.com>
2023-06-26 16:02:52 +0300

    first test

--- src/test/java/tests/ParametrizedTests.java
+++ src/test/java/tests/ParametrizedTests.java
@@ -1,4 +1,19 @@
 package tests;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selenide.*;
+
 public class ParametrizedTests  extends ParametrizedTestBase {
+
+    @ValueSource(strings = {"Пицца", "Закуски"})
+    @ParameterizedTest(name = "Отображение заголовка при переходе в меню {0}")
+    void headerMenuValues(String value) {
+        open(baseUrl);
+        $$("a[data-menu-block]").findBy(text(value)).click();
+        $(".title").shouldHave(text(value));
+    }
 }
